2038c7ef048011ac0ac1d2fe51220d1b3c5325a8
Bin Cui <bin.cui@rea-group.com>
2020-01-30 20:47:25 +0800

    Add ratelimiter

---
+++ src/main/java/com/airtasker/challenge/ratelimiter/core/LazyRefillRateLimiter.java
@@ -0,0 +1,76 @@
+package com.airtasker.challenge.ratelimiter.core;
+
+import java.time.Clock;
+
+public class LazyRefillRateLimiter {
+
+  private int remainTokens;
+
+  private int maxTokens;
+
+  private long lastRefillTime;
+
+  private static final int DEFAULT_TOKEN_PER_REQUEST = 1;
+
+  private Clock clock;
+
+  public LazyRefillRateLimiter(int maxTokens) {
+    this.maxTokens = maxTokens;
+    this.remainTokens = maxTokens;
+    this.clock = Clock.systemDefaultZone();
+    this.lastRefillTime = clock.millis();
+  }
+
+  public LazyRefillRateLimiter(int maxTokens, Clock clock) {
+    this.maxTokens = maxTokens;
+    this.remainTokens = maxTokens;
+    this.clock = clock;
+    this.lastRefillTime = clock.millis();
+  }
+
+  public boolean allow() {
+    return allow(DEFAULT_TOKEN_PER_REQUEST);
+  }
+
+  public boolean allow(int tokenNeeded) {
+    synchronized (this) {
+      refillTokens();
+      if (remainTokens <= 0 || remainTokens < tokenNeeded) {
+        return false;
+      }
+      remainTokens = remainTokens - tokenNeeded;
+      return true;
+    }
+  }
+
+  private void refillTokens() {
+
+    long currentTime = clock.millis();
+    double durationSinceLastRefill = (currentTime - lastRefillTime) / 1000.0;
+
+    int cnt = (int) (durationSinceLastRefill / 3600 * maxTokens);
+
+    if (cnt > 0) {
+      remainTokens = Math.min(remainTokens + cnt, maxTokens);
+      lastRefillTime = currentTime;
+    }
+  }
+
+  public long waitTime() {
+    return waitTime(DEFAULT_TOKEN_PER_REQUEST);
+  }
+
+  public long waitTime(int tokensNeeded) {
+    if (remainTokens >= tokensNeeded) {
+      return 0;
+    }
+    long singleTokenWaitTime = 60 * 60 * 1000 / maxTokens;
+
+    long currentTime = clock.millis();
+
+    long durationSinceLastRefill = (currentTime - lastRefillTime);
+
+    return singleTokenWaitTime * (tokensNeeded - remainTokens) - durationSinceLastRefill;
+  }
+
+}
---
+++ src/test/java/com/airtasker/challenge/ratelimiter/core/LazyRefillRateLimiterTest.java
@@ -0,0 +1,60 @@
+package com.airtasker.challenge.ratelimiter.core;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+
+
+class LazyRefillRateLimiterTest {
+
+  @Test
+  public void allowWithParameterShouldReturnTrueWhenThereIsEnoughTokenLeft() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(2);
+    assertThat(lazyRefillRateLimiter.allow(1), Is.is(true));
+  }
+
+  @Test
+  public void allowWithParameterShouldReturnFalseWhenThereIsNotEnoughTokenLeft() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(2);
+    assertThat(lazyRefillRateLimiter.allow(3), Is.is(false));
+  }
+
+  @Test
+  public void allowWithParameterShouldReturnFalseWhenInitialTokenIsZero() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(0);
+    assertThat(lazyRefillRateLimiter.allow(), Is.is(false));
+  }
+
+  @Test
+  public void allowWithNoParameterShouldReturnTrueWhenThereIsEnoughTokenLeft() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(2);
+    assertThat(lazyRefillRateLimiter.allow(), Is.is(true));
+  }
+
+  @Test
+  public void waitTimeWithParameterShouldReturnZeroWhenThereIsEnoughToken() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(10);
+    assertThat(lazyRefillRateLimiter.waitTime(1), Is.is(0L));
+  }
+
+  @Test
+  public void waitTimeWithParameterShouldReturnWaitTimeWhenThereIsNotEnoughToken() {
+    String instantExpected = "2020-01-31T10:15:30Z";
+    Clock clock = Clock.fixed(Instant.parse(instantExpected), ZoneId.of("UTC"));
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(10, clock);
+    assertThat(lazyRefillRateLimiter.waitTime(11), Is.is(360000L));
+  }
+
+  @Test
+  public void waitTimeWithNoParameterShouldReturnWaitTimeWhenThereIsNotEnoughToken() {
+    LazyRefillRateLimiter lazyRefillRateLimiter = new LazyRefillRateLimiter(1);
+    assertThat(lazyRefillRateLimiter.waitTime(), Is.is(0L));
+  }
+
+}
+
+
